60895342cc83d6cc35c595d13ea94c3534d06b80
gaelserranoaven <gael.serranoaven@gmail.com>
2026-09-10 11:25:04 -0500

    Update supabase.js
    cambio de supabase

--- js/supabase.js
+++ js/supabase.js
@@ -2,7 +2,7 @@
    FINZY — Configuración de Supabase
    =================================================================== */
 
-const SUPABASE_URL = 'https://jpkouhhjyxnxxzkyiugt.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_FqIb7ddYD6HR400og3uFNw_etvxeJb_';
+const SUPABASE_URL = 'https://kssqecmkouqojbcwudxz.supabase.co';
+const SUPABASE_KEY = 'sb_publishable__zch_FyhCZ6j37NQr0Ddbg_9kqZPVJT';
 
 const _supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
